1941743b55af4ca5b1abe2c8f9f71b206f155ab9
sL0p3 <shlokjakhotia@gmail.com>
2022-09-29 18:36:26 +0530

    updated

---
+++ Step_3/3.1/UnionOfSortedArrays.cpp
@@ -0,0 +1,104 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<bool> vb;
+#define sqr(x) ((LL)(x) * (x))
+#define endl "\n"
+#define pb push_back
+#define pp pop_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define alla(arr, sz) arr, arr + sz
+#define setbits(x) __builtin_popcount(x)
+#define unsetbits(x) (int)log2(x)-setbits(x)+1
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(unordered_map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fi); cerr << ","; _print(p.se); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "{ "; for (T i : v) {_print(i); cerr << " ";} cerr << "}";}
+template <class T> void _print(unordered_set <T> v) {cerr << "{ "; for (T i : v) {_print(i); cerr << " ";} cerr << "}";}
+template <class T> void _print(multiset <T> v) {cerr << "{ "; for (T i : v) {_print(i); cerr << " ";} cerr << "}";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+const int M = 1e9 + 7;
+const int P = 1e8 + 10;
+// vb isPrime(P, true);
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+ll binMul(ll a ,ll b){ll ans = 0 ;while(b){if (b&1){ans = (ans+  a)%M;}a = (a+a)%M;b>>= 1;}return ans ;}
+ll binExp(ll a, ll b, int m) {a %= m;ll ans = 1;while (b) {if (b & 1) ans = binMul(ans,a);a = binMul(a,a);b = b >> 1;}return ans;}
+// void sieve(){isPrime[0]=isPrime[1]=false;for(int i = 2; i*i < P; i++){if(isPrime[i]==true){ for(int j = i * i; j < P; j += i)isPrime[j] = false;}}}
+ll gcd(ll a, ll b) {if (a == 0) return b; return gcd(b % a, a);}
+ll lcm(ll a, ll b) {return ((a * b) / (gcd(a, b)));}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+vi findUnion(vi arr1, vi arr2, int n, int m){
+    int i = 0, j = 0;
+    vi Union ;
+    while (i<n and j<m){
+        if(arr1[i]<=arr2[j]){
+            if(Union.size()==0 or Union.back() != arr1[i]) Union.pb(arr1[i]);
+            i++;
+        }else if(arr1[i] > arr2[j]) {
+            if(Union.size()==0 or Union.back() != arr2[j]) Union.pb(arr2[j]);
+            j++;
+        }
+    }
+    while(i<n){
+        if(Union.back() != arr1[i]) Union.pb(arr1[i]);
+        i++;
+    }
+    while(j<m){
+        if(Union.back() != arr2[j]) Union.pb(arr2[j]);
+        j++;
+    }
+    return Union;
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    fastio();
+
+    int n,m;cin>>n>>m;
+    vi arr1(n),arr2(m);
+    for(int i= 0 ;i<n;i++) cin>>arr1[i];
+    for(int i= 0 ;i<m;i++) cin>>arr2[i];
+    vi Union = findUnion(arr1, arr2, n,m);
+    for(auto &it : Union) cout<<it<<" ";
+    return 0;
+}
+
+/* Shlok Jakhotia */
